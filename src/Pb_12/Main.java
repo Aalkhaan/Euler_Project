f078b9f4535aad14627100da00cb9c7099470a05
Guillaume Roussel <71261562+Aalkhaan@users.noreply.github.com>
2021-11-27 12:35:16 +0100

    Pb 11

--- src/Pb_12/Main.java
+++ src/Pb_12/Main.java
@@ -2,5 +2,21 @@ package Pb_12;
 
 public class Main {
     public static void main(String[] args) {
+        long triangleNumber = 6;
+        int natural = 3;
+        long evenNumber = 4;
+        int nbDivisors = 4;
+        while (nbDivisors < 500) {
+            evenNumber <<= 1;
+            //triangleNumber = evenNumber * (evenNumber - 1) >> 1;
+            triangleNumber += natural;
+            nbDivisors = 2;
+            for (long i = 2; i < triangleNumber; i++) {
+                if (triangleNumber % i == 0) {
+                    nbDivisors++;
+                }
+            }
+        }
+        System.out.println(triangleNumber);
     }
 }
